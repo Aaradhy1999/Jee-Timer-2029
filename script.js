f928e05e5f42f4ac5e3063387e41860f678a8ea1
Aaradhy1999 <aaradhy.agarwal1999@gmail.com>
2026-06-26 15:25:05 +0530

    Defined the Target Date milestone

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const daysContainer = document.getElementById('days');
 const hoursContainer = document.getElementById('hours');
 const minutesContainer = document.getElementById('minutes');
 const secondsContainer = document.getElementById('seconds');
+
+const targetDate = new Date('January 1, 2029 00:00:00').getTime();
