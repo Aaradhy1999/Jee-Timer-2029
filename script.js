31235c0721b1de0850301d3f0c5fdc4f90fa7d00
Aaradhy1999 <aaradhy.agarwal1999@gmail.com>
2026-06-26 13:44:34 +0530

    initialized core DOM element selection variables for countdown layout metrics

---
+++ script.js
@@ -0,0 +1,5 @@
+const monthsContainer = document.getElementById('months');
+const daysContainer = document.getElementById('days');
+const hoursContainer = document.getElementById('hours');
+const minutesContainer = document.getElementById('minutes');
+const secondsContainer = document.getElementById('seconds');
